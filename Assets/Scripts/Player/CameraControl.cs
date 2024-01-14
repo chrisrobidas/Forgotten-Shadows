23efdd6273762de078de259e2782da89dfeca5ee
Master-YEET <christopher.robidas@outlook.com>
2024-01-13 19:28:06 -0500

    Added camera collision

--- Assets/Scripts/Player/CameraControl.cs
+++ Assets/Scripts/Player/CameraControl.cs
@@ -16,10 +16,12 @@ public class CameraControl : MonoBehaviour
 
     private float _mouseX;
     private float _mouseY;
+    private Vector3 _cameraInitialLocalPosition;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        _cameraInitialLocalPosition = transform.localPosition;
     }
 
     private void FixedUpdate()
@@ -31,13 +33,26 @@ public class CameraControl : MonoBehaviour
     {
         _mouseX += Input.GetAxis("Mouse X") * _rotationSpeed;
         _mouseY -= Input.GetAxis("Mouse Y") * _rotationSpeed;
-        _mouseY = Mathf.Clamp(_mouseY, -35, 60);
+        _mouseY = Mathf.Clamp(_mouseY, -60, 60);
 
-        Quaternion cameraFollowTargetRotation = Quaternion.Euler(_mouseY, _mouseX, 0);
-
-        _cameraFollowTarget.rotation = cameraFollowTargetRotation;
+        _cameraFollowTarget.rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
 
         _hipJoint.targetRotation = Quaternion.Euler(0, -_mouseX, 0);
         _stomachJoint.targetRotation = Quaternion.Euler(-_mouseY, 0, 0);
+
+        int layerMask = ~(1 << LayerMask.NameToLayer("NoSelfCollision"));
+
+        Debug.DrawLine(_cameraFollowTarget.position, _cameraFollowTarget.position + _cameraFollowTarget.localRotation * _cameraInitialLocalPosition, Color.red);
+
+        if (Physics.Linecast(_cameraFollowTarget.position, _cameraFollowTarget.position + _cameraFollowTarget.localRotation * _cameraInitialLocalPosition, out RaycastHit hit, layerMask))
+        {
+            transform.position = hit.point + _cameraFollowTarget.localRotation * new Vector3(0, 0, 0.1f);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _cameraInitialLocalPosition, Time.deltaTime);
+        }
+
+        transform.LookAt(_cameraFollowTarget);
     }
 }
